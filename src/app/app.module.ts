a2c5cd9b259acba8d21760b0719cc87a746e94b6
Iván <ivantbd87@gmail.com>
2023-07-15 10:09:17 +0200

    Tasks project created, first steps creating components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { TareaComponent } from './components/tarea/tarea.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    TareaComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/tarea/tarea.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tarea',
+  templateUrl: './tarea.component.html',
+  styleUrls: ['./tarea.component.css']
+})
+export class TareaComponent {
+
+}
